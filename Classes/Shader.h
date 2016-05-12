26d4906c9bc1d872dee44fecfe7e850bbb9fc536
april@netease <yi.wang@HIH-D-4436.hz.ntes.domain>
2016-05-12 15:27:03 +0800

    dl unrelated files

--- Classes/Shader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "stdafx.h"
-class ShaderProgram
-{
-public:
-	ShaderProgram();
-	~ShaderProgram();
-	GLuint InitShader(const char* vertex_shader_file, const char* fragment_shader_file);
-};
